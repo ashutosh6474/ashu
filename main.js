6d58ffbcedad5d82fbdc7fe2f7ae1d758acf80ee
ashutosh6474 <chaturvediashutosh619@gmail.com>
2026-03-13 17:29:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+// when the user scrolls the page execute myfunction
+window.onscroll= function () {myFunction() };
+
+function myFunction(){
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled +"%";
+}
+
+
+ 
+    var btn = $('#button');
+
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+
+    btn.on('click', function (e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: 0 }, '300');
+    });
+
+
+  
+
+
